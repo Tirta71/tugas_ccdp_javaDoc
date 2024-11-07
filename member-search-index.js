564e9090c5d618c7c46fd62a59fb9ff465ffc6ee
tirta71 <tirta4132@gmail.com>
2024-11-07 15:42:55 +0700

    first commit

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Kalkulator","l":"Kalkulator()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Kalkulator","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
